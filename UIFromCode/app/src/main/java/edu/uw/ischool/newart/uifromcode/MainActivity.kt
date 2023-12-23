ac46143e10492ab9fcd16c9ade456295769989bc
Ted Neward <ted@tedneward.com>
2023-12-22 23:30:27 -0800

    Simplifying and adding a bit more

--- UIFromCode/app/src/main/java/edu/uw/ischool/newart/uifromcode/MainActivity.kt
+++ UIFromCode/app/src/main/java/edu/uw/ischool/newart/uifromcode/MainActivity.kt
@@ -2,8 +2,11 @@ package edu.uw.ischool.newart.uifromcode
 
 import android.R
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -14,19 +17,43 @@ import androidx.core.view.updateLayoutParams
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Nope, not using resources this time, sorry bud
         //setContentView(R.layout.activity_main)
 
+        //*
+        //{{## BEGIN simple-vertical ##}}
         // Simple Vertical LinearLayout
         //
-        /*
         val viewRoot = LinearLayout(this)
         viewRoot.orientation = LinearLayout.VERTICAL
         viewRoot.layoutParams =
             ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT)
-         */
 
+        val textView = TextView(this)
+        textView.id = View.generateViewId()
+        textView.text = "Hello, Android!"
+        textView.gravity = Gravity.CENTER_HORIZONTAL
+        textView.layoutParams =
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+        viewRoot.addView(textView)
+
+        val btnPushMe = Button(this)
+        btnPushMe.id = View.generateViewId()
+        btnPushMe.text = "Push Me!"
+        btnPushMe.layoutParams =
+                ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+        viewRoot.addView(btnPushMe)
+        //{{## END simple-vertical ##}}
+        // */
+
+        /*
+        // {{## BEGIN constraintLayout ##}}
         // More complex ConstraintLayout
         //
         val viewRoot = ConstraintLayout(this)
@@ -50,9 +77,12 @@ class MainActivity : AppCompatActivity() {
             startToStart = viewRoot.id
             endToEnd = viewRoot.id
         }
-
         viewRoot.addView(textView)
+        // {{## END constraintLayout ##}}
+        // */
 
+        // {{## BEGIN setRoot ##}}
         setContentView(viewRoot)
+        // {{## END setRoot ##}}
     }
 }
